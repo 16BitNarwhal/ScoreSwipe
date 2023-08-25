c46d94448e8ef985b68e7cfc4203f305851895ab
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2023-08-25 14:35:39 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# pdfplayer
+
+Flips PDF pages by tracking head movement using the device's camera.
+
+## Todo
+
+- [ ] Pick file (.pdf)
+- [ ] View PDF
+- [ ] MLKit
+  - [ ] Face detection
+  - [ ] Head tracking
+- [ ] Flip pages
+- [ ] Settings (hardcoded)
+  - [ ] Flip sensitivity
+  - [ ] Flip direction
+  - [ ] Flip mode (auto/manual)
+  - [ ] Flip mode (head movement/eye blink)
+- [ ] UI ...
+
+## Resources
+
+- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
+- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
+
+For help getting started with Flutter development, view the
+[online documentation](https://docs.flutter.dev/), which offers tutorials,
+samples, guidance on mobile development, and a full API reference.
---
+++ lib/main.dart
@@ -0,0 +1,81 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'PDF Player Dev',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const HomePage(title: 'File Picker'),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _fileText = "";
+
+  void _pickFile() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+        type: FileType.custom,
+        allowedExtensions: ['pdf'],
+        allowMultiple: false);
+
+    if (result != null && result.files.single.path != null) {
+      // PlatformFile file = result.files.first;
+      // print(file.name);
+      // print(file.size);
+      // print(file.extension);
+      // print(file.path);
+
+      File _file = File(result.files.single.path!);
+      setState(() {
+        _fileText = _file.path;
+      });
+    } else {
+      // User canceled the picker
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'Pick a file below:',
+            ),
+            ElevatedButton(
+                onPressed: _pickFile, child: const Text('Pick File')),
+            Text(_fileText),
+          ],
+        ),
+      ),
+    );
+  }
+}
