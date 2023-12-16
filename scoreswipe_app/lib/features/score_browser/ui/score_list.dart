0a286c4b2e19f09823b2f753ba0263f3be7b817c
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2023-12-16 17:14:08 -0500

    UI spacing fix and list minimum height

--- scoreswipe_app/lib/features/score_browser/ui/score_list.dart
+++ scoreswipe_app/lib/features/score_browser/ui/score_list.dart
@@ -18,66 +18,72 @@ class MusicSheetsView extends StatelessWidget {
           color: Theme.of(context).colorScheme.background,
           borderRadius: const BorderRadius.vertical(top: Radius.circular(30)),
         ),
-        padding: const EdgeInsets.all(32),
+        padding: const EdgeInsets.symmetric(horizontal: 32),
         child: SingleChildScrollView(
           physics: const BouncingScrollPhysics(),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-            children: [
-              Row(
-                children: [
-                  Text(
-                    'Scores',
-                    style: TextStyle(
-                        fontSize: 18,
-                        color: Theme.of(context).colorScheme.onBackground,
-                        fontWeight: FontWeight.w700),
-                  ),
-                  const Spacer(),
-                  Text(
-                    'Sort Most Recent',
-                    style: TextStyle(
-                        fontSize: 14,
-                        color: Theme.of(context).colorScheme.onBackground,
-                        fontWeight: FontWeight.w400),
-                  ),
-                  IconButton(
-                    icon: Icon(Icons.arrow_drop_down,
-                        size: 32,
-                        color: Theme.of(context).colorScheme.onBackground),
-                    onPressed: () {
-                      // Handle sort icon tap
-                    },
-                  ),
-                ],
-              ),
-              BlocBuilder<ScoreBrowserBloc, ScoreBrowserState>(
-                bloc: ScoreBrowserBloc()..add(LoadScores()),
-                builder: (context, state) {
-                  return state.scores.isEmpty
-                      ? const Center(
-                          child: Text(
-                            'No scores found',
-                            style: TextStyle(
-                                fontSize: 18,
-                                color: Colors.grey,
-                                fontWeight: FontWeight.w700),
-                          ),
-                        )
-                      : Column(
-                          children: [
-                            for (ScoreModel score in state.scores)
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(vertical: 8.0),
-                                child: MusicSheetCard(
-                                    score: score, refresh: refresh),
-                              ),
-                          ],
-                        );
-                },
-              ),
-            ],
+          child: ConstrainedBox(
+            constraints:
+                BoxConstraints(minHeight: MediaQuery.of(context).size.height),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                const SizedBox(height: 32),
+                Row(
+                  children: [
+                    Text(
+                      'Scores',
+                      style: TextStyle(
+                          fontSize: 18,
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontWeight: FontWeight.w700),
+                    ),
+                    const Spacer(),
+                    Text(
+                      'Sort Most Recent',
+                      style: TextStyle(
+                          fontSize: 14,
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    IconButton(
+                      icon: Icon(Icons.arrow_drop_down,
+                          size: 32,
+                          color: Theme.of(context).colorScheme.onBackground),
+                      onPressed: () {
+                        // Handle sort icon tap
+                      },
+                    ),
+                  ],
+                ),
+                BlocBuilder<ScoreBrowserBloc, ScoreBrowserState>(
+                  bloc: ScoreBrowserBloc()..add(LoadScores()),
+                  builder: (context, state) {
+                    return state.scores.isEmpty
+                        ? const Center(
+                            child: Text(
+                              'No scores found',
+                              style: TextStyle(
+                                  fontSize: 18,
+                                  color: Colors.grey,
+                                  fontWeight: FontWeight.w700),
+                            ),
+                          )
+                        : Column(
+                            children: [
+                              for (ScoreModel score in state.scores)
+                                Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(vertical: 8.0),
+                                  child: MusicSheetCard(
+                                      score: score, refresh: refresh),
+                                ),
+                            ],
+                          );
+                  },
+                ),
+                const SizedBox(height: 32),
+              ],
+            ),
           ),
         ),
       ),
