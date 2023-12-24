93a465b68dc242e5386c660acd1355ed1b13c214
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2023-12-23 23:49:47 -0500

    import pdf from creator_screen

--- scoreswipe_app/lib/features/score_creator/ui/score_creator_screen.dart
+++ scoreswipe_app/lib/features/score_creator/ui/score_creator_screen.dart
@@ -1,10 +1,15 @@
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
 import 'package:file_picker/file_picker.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:pdfplayer/common/data/local_score_repository.dart';
+import 'package:pdf_render/pdf_render.dart';
 import 'dart:io';
+import 'dart:ui' as ui;
+import 'package:path_provider/path_provider.dart';
 
 import 'package:pdfplayer/features/score_browser/bloc/score_browser_bloc.dart';
+
+import 'package:logger/logger.dart';
 
 class ScoreCreatorScreen extends StatefulWidget {
   const ScoreCreatorScreen({super.key});
@@ -17,6 +22,23 @@ class _ScoreCreatorScreenState extends State<ScoreCreatorScreen> {
   List<File> images = [];
   int currentPage = 0;
   String scoreName = '';
+  late Directory dir;
+
+  @override
+  void initState() {
+    super.initState();
+    dir = Directory.systemTemp.createTempSync();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    dir.delete(recursive: true);
+  }
+
+  Future<void> openCamera() async {
+    //
+  }
 
   Future<void> pickImages() async {
     FilePickerResult? result = await FilePicker.platform.pickFiles(
@@ -24,13 +46,39 @@ class _ScoreCreatorScreenState extends State<ScoreCreatorScreen> {
       allowMultiple: true,
     );
 
-    if (result != null) {
-      List<File> pickedImages =
-          result.paths.map((path) => File(path!)).toList();
-      setState(() {
-        images.addAll(pickedImages);
-      });
+    if (result != null) return;
+    List<File> pickedImages = result!.paths.map((path) => File(path!)).toList();
+    setState(() {
+      images.addAll(pickedImages);
+    });
+  }
+
+  Future<void> pickPdf() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.custom,
+      allowedExtensions: ['pdf'],
+    );
+
+    if (result == null) return;
+
+    File pdfFile = File(result.files.single.path!);
+    PdfDocument pdfDocument = await PdfDocument.openFile(pdfFile.path);
+    List<File> pickedImages = [];
+
+    for (int i = 1; i <= pdfDocument.pageCount; i++) {
+      PdfPage page = await pdfDocument.getPage(i);
+      PdfPageImage pdfImage = await page.render();
+      ui.Image image = await pdfImage.createImageDetached();
+      ByteData? imgBytes =
+          await image.toByteData(format: ui.ImageByteFormat.png);
+      File imageFile = File('${dir.path}/$i.png');
+      await imageFile.writeAsBytes(imgBytes!.buffer.asUint8List());
+      pickedImages.add(imageFile);
     }
+
+    setState(() {
+      images.addAll(pickedImages);
+    });
   }
 
   bool isValid() {
@@ -80,14 +128,33 @@ class _ScoreCreatorScreenState extends State<ScoreCreatorScreen> {
               currentPage: currentPage,
               totalPages: images.length,
             ),
-            // add from camera, add from gallery, import pdf
-            ElevatedButton(
-              onPressed: pickImages,
-              style: ElevatedButton.styleFrom(
-                foregroundColor: Colors.white,
-                backgroundColor: Colors.blue,
-              ),
-              child: const Text('Add Images'),
+            Row(
+              children: [
+                ElevatedButton(
+                  onPressed: openCamera,
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text('Add From Camera'),
+                ),
+                ElevatedButton(
+                  onPressed: pickImages,
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text('Add Images'),
+                ),
+                ElevatedButton(
+                  onPressed: pickPdf,
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white,
+                    backgroundColor: Colors.blue,
+                  ),
+                  child: const Text('Import PDF'),
+                ),
+              ],
             ),
             Container(
               margin: const EdgeInsets.all(16),
