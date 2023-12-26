ec4b0e3e7905fa2f48ce6f6195dda631856c5cc6
Eric <31218485+16BitNarwhal@users.noreply.github.com>
2023-12-26 01:04:18 -0500

    removed equatable in state (due to list issue described in issue #14)

--- scoreswipe_app/lib/features/score_browser/bloc/score_browser_state.dart
+++ scoreswipe_app/lib/features/score_browser/bloc/score_browser_state.dart
@@ -1,25 +1,16 @@
 part of 'score_browser_bloc.dart';
 
 @immutable
-sealed class ScoreBrowserState extends Equatable {
+sealed class ScoreBrowserState {
   final List<ScoreModel> scores;
 
   const ScoreBrowserState({this.scores = const []});
-
-  @override
-  List<Object> get props => [];
 }
 
 final class ScoreBrowserLoading extends ScoreBrowserState {
   const ScoreBrowserLoading();
-
-  @override
-  List<Object> get props => [];
 }
 
 final class ScoreBrowserLoaded extends ScoreBrowserState {
   const ScoreBrowserLoaded({required super.scores});
-
-  @override
-  List<Object> get props => [scores];
 }
